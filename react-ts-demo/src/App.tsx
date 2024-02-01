bbce9ed819709901c02e1a9e434e9165ec70844f
Rainafgalaxy <qihui.yang@myflashcloud.com>
2024-02-01 16:56:56 +0800

    none

--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -4,6 +4,10 @@ import './App.css';
 
 function App() {
   return (
+    // 所有jsx语法中，标签必须要闭合，每个jsx片段中只能有一个根节点
+    // jsx语法中，
+    // 1.class要写为className(因为class是一个关键字)
+    // 2.内嵌样式style，首先要写两个花括号，然后属性名用驼峰式命名,要用对象的形式(属性值要加引号)
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -15,6 +19,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{color:"pink",backgroundColor:"skyblue"}}
         >
           Learn React
         </a>
