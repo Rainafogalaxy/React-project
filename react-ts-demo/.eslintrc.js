28b4e37013df20f7ed4e581b0f7be41c51c471a9
Azi <yuwuyinhe@163.com>
2024-02-01 21:25:02 +0800

    none

--- react-ts-demo/.eslintrc.js
+++ react-ts-demo/.eslintrc.js
@@ -1,23 +1,20 @@
 module.exports = {
-    "env":{
-        "browser":true,
-        "es2021":true
-    },
-    "extends":[
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-    ],
-    "overrides":[],
-    "paeser": "@typescript-eslint/parser",
-    "parserOptions":{
-        "exmaVersion":"latest",
-        "sourceType":"module"
-    },
-    "plugins":[
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules":{}
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    exmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
