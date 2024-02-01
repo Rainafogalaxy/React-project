bb01a0549f47689a854c9da5b490b0ab73e3fc27
Azi <yuwuyinhe@163.com>
2024-02-01 23:53:11 +0800

    nnn

--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import List1 from "./List1";
+import { useState } from "react";
+// import List1 from "./List1";
 
 function App() {
+  let [love, setLove] = useState(100);
+  function add() {
+    setLove(love + 10);
+  }
   return (
     <>
-      <List1 />
+      {/* <List1 /> */}
+      <div>
+        <button onClick={add}>Love KyungSoo{love}%</button>
+      </div>
     </>
   );
 }
